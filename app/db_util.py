029afee7d9c52d5c068cbb1b7218db67428ce42a
Lohith <lohith@Lohiths-MBP.cable.rcn.com>
2019-03-26 18:55:34 -0400

    Test commit

--- app/db_util.py
+++ app/db_util.py
@@ -8,21 +8,16 @@ class Data(object):
         self.engine = db.db_engine
         self.table_name = table_name
         self.df = pd.read_sql(self.table_name, self.engine)
-    
+        self.keys = list(set([key[0] for key in self.df.keys()]))
         self.supported_attributes = None
         self.id_to_name = None
         
         def create_pivoted_table(self):
             self.df = pd.pivot_table(self.df, index='id',columns='year', values=list(set(self.df.keys()-set(['year', 'id'])))
-            keys = list(set([key[0] for key in self.df.keys()]))
-            for i, key in enumerate(keys, 1):
+            for i, key in enumerate(self.keys, 1):
                 self.df[key].iloc[:, 0] = self.df[key].iloc[:, 0].fillna(0)
                 self.df[key].iloc[:, -1] = self.df[key].iloc[:, -1].fillna(0)
                 self.df[key] = self.df[key].interpolate(axis=1)
-            print(np.round(i/len(keys), 2), end='\r')
+            print(np.round(i/len(self.keys), 2), end='\r')
 
             print(' DONE !')
-
-
-
-    
